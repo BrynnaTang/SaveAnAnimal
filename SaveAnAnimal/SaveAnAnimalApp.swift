3d21a3d9c994d487ec84b7093a658e28fee6fb6a
Brynna Tang <brynnatang@gmail.com>
2025-07-15 11:32:36 -0400

    Initial Commit

---
+++ SaveAnAnimal/SaveAnAnimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveAnAnimalApp.swift
+//  SaveAnAnimal
+//
+//  Created by Lisa Tang on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SaveAnAnimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
