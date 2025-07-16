0d68a1deb74b86e3b8a826489766cab4beab7f9c
Solary Nava Luna <itzsolnl4@gmail.com>
2025-07-16 14:24:29 -0400

    i fixed the dot dot issue

--- SaveAnAnimal/AnimalSixView.swift
+++ SaveAnAnimal/AnimalSixView.swift
@@ -19,7 +19,7 @@ struct AnimalSixView: View {
                     .aspectRatio(contentMode: .fit)
                 Text("Cheetahs are found in southern and eastern Africa, with a small critically endangered population of cheetahs in Iran. They once sprawled through vast landscapes, cheetahs have adapted to life in many habitats like grasslanda, deserts, etc.")
                     .multilineTextAlignment(.leading)
-                    .lineLimit(15)
+                    .lineLimit(19)
                     
             }
             .padding([.top, .leading, .trailing])
