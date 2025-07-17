dbf1b315831bd29b2f070d14e0e734fca11047bd
Brynna Tang <brynnatang@gmail.com>
2025-07-17 13:41:29 -0400

    fixed small issues with the layout, changed home page

--- SaveAnAnimal/ContentView.swift
+++ SaveAnAnimal/ContentView.swift
@@ -11,153 +11,53 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             VStack {
-            HStack{
-                Text("SAVE AN ANIMAL")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                Spacer()
-            }
+                HStack{
+                    Text("SAVE AN ANIMAL")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Spacer()
+                }
                 HStack{
                     NavigationLink(destination: MissionView()) {
                         Text("Our Mission")
+                            .fontWeight(.bold)
                         
                         Spacer()
-                            .frame(width: 31.0)
+                            .frame(width: 25.0)
                         
                         NavigationLink(destination: ListView()) {
                             Text("Endangered Animals")
+                                .fontWeight(.bold)
                             
-                        Spacer()
+                            Spacer()
                             
                             NavigationLink(destination: DonateView()) {
                                 Text("Donate")
+                                    .fontWeight(.bold)
                             }
                         }
                     }
                 }
                 
-            HStack{
-                Text("OUR MISSION/ABOUT THE APP:")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.leading)
-                Spacer()
-            }
-            HStack{
-                Text("TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT")
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 40.0)
-            
-            HStack{
+                HStack{
+                    Text("OUR MISSION/ABOUT THE APP:")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                }
+                HStack{
+                    Text("TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT")
+                    Spacer()
+                }
                 
-                Text("LIST OF ENDANGERED ANIMALS:")
-                    .fontWeight(.bold)
                 Spacer()
+       
+                
+                
             }
-            Spacer()
-                .frame(height: 10.0)
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalOneView()){
-                    Text("Southern Sea Otter")
-                        .fontWeight(.bold)
-                }
-             
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 5.0)
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalTwoView()){
-                    Text("Sumatran Tiger")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 5.0)
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalThreeView()){
-                    Text("Monarch Butterflies")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 5.0)
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalFourView()){
-                    Text("Sea Turtles")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalFiveView()){
-                    Text("Black Rhino")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalSixView()){
-                    Text("Cheetah")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalSevenView()){
-                    Text("Red Panda")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalEightView()){
-                    Text("Black-Footed Ferret")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            
-            
-            
-            
-            Spacer()
+            .padding()
         }
-        }
-        .padding()
     }
 }
 
