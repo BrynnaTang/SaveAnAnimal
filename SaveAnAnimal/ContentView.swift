2829ce79846ccd9ee97f1d735a7737d4f07ab1a7
Solary Nava Luna <itzsolnl4@gmail.com>
2025-07-17 13:28:24 -0400

    Added "IDK" color on to main screen.

--- SaveAnAnimal/ContentView.swift
+++ SaveAnAnimal/ContentView.swift
@@ -9,136 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            VStack {
-                HStack(){
-                Text("SAVE AN ANIMAL")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                Spacer()
-            }
-            .background(Color("Nature "))
-            HStack{
-                Text("OUR MISSION/ABOUT THE APP:")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.leading)
-                Spacer()
-            }
-            HStack{
-                Text("Our mission is to prevent animals from going extinct and convince people on why they should help.")
-                Spacer()
-            }
+        ZStack{
             
-            Spacer()
-                .frame(height: 40.0)
-            
-            HStack{
                 
-                Text("LIST OF ENDANGERED ANIMALS:")
-                    .fontWeight(.bold)
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 10.0)
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalOneView()){
-                    Text("Southern Sea Otter")
-                        .fontWeight(.bold)
+            NavigationStack{
+                ZStack{
+                    
+                    
+                    VStack {
+                        
+                        Text("SAVE AN ANIMAL")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Spacer()
+                        
+                        
+                        Text("OUR MISSION/ABOUT THE APP:")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                        
+                        HStack{
+                            Text("Our mission is to prevent animals from going extinct and convince people on why they should help.")
+                            Spacer()
+                        }
+                        
+                        Spacer()
+                            .frame(height: 40.0)
+                        
+                        
+                        
+                        Text("LIST OF ENDANGERED ANIMALS:")
+                            .fontWeight(.bold)
+                        Spacer()
+                        
+                        Spacer()
+                        
+                        
+                        //Text("-")
+                        NavigationLink(destination: AnimalOneView()){
+                            Text("Southern Sea Otter")
+                                .fontWeight(.bold)
+                        }
+                        
+                        
+                        Spacer()
+                        NavigationLink(destination: AnimalTwoView()){
+                            Text("Sumatran Tiger")
+                                .fontWeight(.bold)
+                        }
+                        Spacer()
+                        NavigationLink(destination: AnimalThreeView()){
+                            Text("Monarch Butterflies")
+                                .fontWeight(.bold)
+                        }
+                        Spacer()
+                        NavigationLink(destination: AnimalFourView()){
+                            Text("Sea Turtles")
+                                .fontWeight(.bold)
+                        }
+                        Spacer()
+                        Text("-")
+                        NavigationLink(destination: AnimalFiveView()){
+                            Text("Black Rhino")
+                                .fontWeight(.bold)
+                        }
+                        Spacer()
+                        Text("-")
+                        NavigationLink(destination: AnimalSixView()){
+                            Text("Cheetah")
+                                .fontWeight(.bold)
+                        }
+                        Text("-")
+                        NavigationLink(destination: AnimalSevenView()){
+                            Text("ANIMAL SEVEN")
+                        }
+                        Spacer()
+                        Text("-")
+                        NavigationLink(destination: AnimalEightView()){
+                            Text("ANIMAL EIGHT")
+                        }
+                        Spacer()
+                    }
+                    
                 }
-             
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 5.0)
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalTwoView()){
-                    Text("Sumatran Tiger")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 5.0)
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalThreeView()){
-                    Text("Monarch Butterflies")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 5.0)
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalFourView()){
-                    Text("Sea Turtles")
-                        .fontWeight(.bold)
-                }
-                Spacer()
+                .background(Color("IDK"))
+                
             }
             
-            Spacer()
-                .frame(height: 5.0)
-            
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalFiveView()){
-                    Text("Black Rhino")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalSixView()){
-                    Text("Cheetah")
-                        .fontWeight(.bold)
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalSevenView()){
-                    Text("ANIMAL SEVEN")
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            HStack {
-                Text("-")
-                NavigationLink(destination: AnimalEightView()){
-                    Text("ANIMAL EIGHT")
-                }
-                Spacer()
-            }
-            
-            Spacer()
-                .frame(height: 5.0)
-            
-            
-            
-            
-            
-            Spacer()
         }
-        }
-        .padding()
     }
+        
+        
 }
 
 #Preview {
